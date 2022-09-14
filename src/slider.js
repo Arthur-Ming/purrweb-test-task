4ae4af599bda5cdf8d7ec55f23ab9168ff8f4ffb
Artur Mingazhov <arthurming7@gmail.com>
2022-09-14 17:53:37 +0300

    feat: add adaptive

--- src/slider.js
+++ src/slider.js
@@ -2,14 +2,16 @@ import Slider from './components/Slider';
 
 const sliderRoot = document.getElementById('slider-root');
 
-function slidesCreater(slidesNum /*number*/) {
+function createSlides(slidesNum /*number*/) {
   return Array.from(Array(slidesNum), (_, index) => ({
     id: index + 1,
     value: index + 1,
   }));
 }
 
-const slides = slidesCreater(5);
+const defaultSlidesNum = 5;
+
+const slides = createSlides(defaultSlidesNum);
 
 if (sliderRoot) {
   sliderRoot.append(new Slider({ slides, delay: 600 }).render());
